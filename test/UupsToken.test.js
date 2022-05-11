b98619dd431364786490950d18ae40a9928df294
fabianschu <48454910+fabianschu@users.noreply.github.com>
2022-05-11 14:38:12 +0200

    admin ownership can be transferred

--- test/UupsToken.test.js
+++ test/UupsToken.test.js
@@ -2,23 +2,57 @@ const { expect } = require("chai");
 const { ethers, upgrades } = require("hardhat");
 
 describe("UupsToken", function () {
-  let deployer;
+  let deployer, alice;
   let uupsToken;
 
   beforeEach("deploy UupsToken", async () => {
-    [deployer] = await ethers.getSigners();
+    [deployer, alice] = await ethers.getSigners();
     const UupsToken = await ethers.getContractFactory("UupsToken");
     uupsToken = await upgrades.deployProxy(UupsToken, { kind: "uups" });
   });
 
   context("when upgrading the token", () => {
+    let uupsTokenV2;
+
     beforeEach("upgrade the token contract", async () => {
       const UupsTokenV2 = await ethers.getContractFactory("UupsTokenV2");
-      uupsToken = await upgrades.upgradeProxy(uupsToken.address, UupsTokenV2);
+      uupsTokenV2 = await upgrades.upgradeProxy(uupsToken.address, UupsTokenV2);
     });
 
     it("the upgraded token contains newly added fn", async () => {
-      await expect(uupsToken.doNothing()).to.emit(uupsToken, "DoNothing");
+      await expect(uupsTokenV2.doNothing()).to.emit(uupsTokenV2, "DoNothing");
+    });
+  });
+
+  context("with proxy admin ownership transferred to alice", () => {
+    beforeEach("transfer proxy admin ownership to alice", async () => {
+      await uupsToken.transferOwnership(alice.address);
+    });
+
+    context("when original deployer tries to upgrade", () => {
+      it("reverts 'Ownable: caller is not the owner'", async () => {
+        const UupsTokenV2 = await ethers.getContractFactory("UupsTokenV2");
+
+        await expect(
+          upgrades.upgradeProxy(uupsToken.address, UupsTokenV2)
+        ).to.be.revertedWith("Ownable: caller is not the owner");
+      });
+    });
+
+    context("when alice tries to upgrade", () => {
+      it("upgrades the implementation", async () => {
+        const UupsTokenV2 = await ethers.getContractFactory(
+          "UupsTokenV2",
+          alice
+        );
+
+        const uupsTokenV2 = await upgrades.upgradeProxy(
+          uupsToken.address,
+          UupsTokenV2
+        );
+
+        await expect(uupsTokenV2.doNothing()).to.emit(uupsTokenV2, "DoNothing");
+      });
     });
   });
 });
